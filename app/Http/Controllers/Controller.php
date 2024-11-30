60d8bd002a72ba81ec09c9d3808744cbbd4ae4b6
Erick leonardo cruz gil <erickdeathsoul@outlook.com>
2024-11-29 20:12:39 -0600

    Subi proyecto completo API con documentacion
    Se subio el proyecto completo de API

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -1,6 +1,16 @@
 <?php
 
 namespace App\Http\Controllers;
+use OpenApi\Attributes as OA;
+
+#[
+    OA\Info(version:"1.0.0",description: "API Desarrollada por Erick Leonardo Cruz Gil para la UFG", title:"Documentacion de API de Erick Leonardo Cruz Gil"),
+    OA\Server(url:"http://localhost:8000",description:"Servidor Local de Desarrollo"),
+    OA\Server(url:"http://localhost:8000",description:"Servidor para pruebas"),
+    OA\Server(url:"http://localhost:8000",description:"Servidor de Producción"),
+    OA\SecurityScheme(securityScheme:"bearerAuth",type:"http",name:"Authorization", in:"header",scheme:"bearer")
+
+]
 
 abstract class Controller
 {
